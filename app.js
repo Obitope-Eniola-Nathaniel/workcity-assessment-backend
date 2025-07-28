8c4635448f3449da2fa499bc7bde473f80a76a44
obitopeeniola <obitopeeniola@gmail.com>
2025-07-28 20:26:14 +0100

    Auth Route

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+// Initialize Express app
+const app = express();
+
+// Import routes
+const authRoutes = require("./routes/auth");
+const errorHandler = require("./middleware/errorHandler");
+
+// Allowed origins for CORS
+const allowedOrigins = ["http://localhost:3000"];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
+// Routes
+app.use("/auth", authRoutes);
+
+// Error handling middleware
+app.use(errorHandler);
+
+// Start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
---
+++ routes/auth.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const { body } = require("express-validator");
+
+const authController = require("../controllers/authController");
+const User = require("../models/User");
+
+const router = express.Router();
+
+//  POST http://localhost:5000/auth/register
+router.post(
+  "/register",
+  [
+    body("email")
+      .isEmail()
+      .withMessage("Invalid email")
+      .custom(async (value) => {
+        const existingUser = await User.findOne({ email: value });
+        if (existingUser) {
+          throw new Error("Email already in use");
+        }
+        return true; // Proceed if no existing user found
+      })
+      .normalizeEmail(),
+    body("password")
+      .trim()
+      .isAlphanumeric()
+      .withMessage("Password must contain only letters and numbers")
+      .isLength({ min: 5 })
+      .withMessage("Password must be at least 5 characters"),
+
+    body("confirmPassword")
+      .trim()
+      .custom((value, { req }) => {
+        if (value !== req.body.password) {
+          throw new Error("Passwords do not match");
+        }
+        return true;
+      }),
+    body("name")
+      .trim()
+      .notEmpty()
+      .withMessage("Name is required")
+      .isLength({ min: 2 })
+      .withMessage("Name must be at least 2 characters long"),
+  ],
+  authController.register
+);
+
+//  POST http://localhost:5000/auth/login
+router.post(
+  "/login",
+  [
+    body("email").isEmail().withMessage("Invalid email"),
+    body("password").exists().withMessage("Password is required"),
+  ],
+  authController.login
+);
+
+module.exports = router;
